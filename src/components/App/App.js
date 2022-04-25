3580fdeb3c4715f3874ec85c957bf3b1893160d2
Avellow <avellow@yandex.ru>
2022-04-25 16:54:25 +0300

    feature создать компонент App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
+import './App.css';
+import Header from "../Header/Header";
 
 function App() {
-  return (
-    <div className="app">
-
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
