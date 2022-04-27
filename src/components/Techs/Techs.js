9ae874b7503154162e5980d4bbdd022188a5eeaf
Avellow <avellow@yandex.ru>
2022-04-27 10:45:00 +0300

    feature верстка компонента Техологии

---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,23 @@
+function Techs() {
+    return (
+        <section className='Techs'>
+           <h2 className='section__title'>Технологии</h2>
+            <div className='Techs__info-container'>
+                <h3 className='Techs__info-title'>7 технологий</h3>
+                <p className='Techs__info-subtitle'>На курсе веб-разработки мы освоили технологии,
+                    которые применили в дипломном проекте.</p>
+                <ul className='Techs__list'>
+                    <li className='Techs__list-item'>HTML</li>
+                    <li className='Techs__list-item'>CSS</li>
+                    <li className='Techs__list-item'>JS</li>
+                    <li className='Techs__list-item'>React</li>
+                    <li className='Techs__list-item'>Git</li>
+                    <li className='Techs__list-item'>Express.js</li>
+                    <li className='Techs__list-item'>mongoDB</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Techs;
