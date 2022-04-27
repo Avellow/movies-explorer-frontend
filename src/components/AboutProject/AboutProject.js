0db118cb4b4c2200a284fd1376ff3154d3fac530
Avellow <avellow@yandex.ru>
2022-04-27 09:56:33 +0300

    feature верстка компонента О проекте

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,27 @@
+function AboutProject() {
+    return (
+        <section className='AboutProject'>
+            <h2 className='section__title'>О проекте</h2>
+            <div className='AboutProject__info-container'>
+                <div className='AboutProject__info'>
+                    <h3 className='AboutProject__info-title'>Дипломный проект включал 5 этапов</h3>
+                    <p className='AboutProject__info-subtitle'>Составление плана, работу над бэкендом,
+                        вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className='AboutProject__info'>
+                    <h3 className='AboutProject__info-title'>На выполнение диплома ушло 5 недель</h3>
+                    <p className='AboutProject__info-subtitle'>У каждого этапа был мягкий и жёсткий дедлайн,
+                        которые нужно было соблюдать,&nbsp;чтобы успешно защититься.</p>
+                </div>
+            </div>
+            <div className='AboutProject__progress-bar'>
+                <div className='AboutProject__back-bar'>1 неделя</div>
+                <div className='AboutProject__front-bar'>4 недели</div>
+                <p className='AboutProject__bar-subtitle'>Back-end</p>
+                <p className='AboutProject__bar-subtitle'>Front-end</p>
+            </div>
+        </section>
+    )
+}
+
+export default AboutProject;
