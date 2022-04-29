fae9383372c45eec3ab23cd1c69a8806aafc2016
Avellow <avellow@yandex.ru>
2022-04-29 14:13:48 +0300

    feature верстка страницы 404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,38 +6,12 @@ import Movies from "../Movies/Movies";
 import Form from "../Form/Form";
 import Auth from "../Auth/Auth";
 import Input from "../Input/Input";
+import NotFound from "../NotFound/NotFound";
 
 function App() {
     return (
         <div className="app">
-
-
-            <Auth>
-                <Form
-                    title='Добро пожаловать!'
-                    buttonText='Зарегистрироваться'
-                    hintText='Уже зарегистрированы?'
-                    hintLinkText='Войти'
-                >
-                    <Input name='Имя' />
-                    <Input name='E-mail' type='email'/>
-                    <Input name='Пароль' type='password' error={true}/>
-                </Form>
-            </Auth>
-
-            <Auth>
-                <Form
-                    title='Рады видеть!'
-                    buttonText='Войти'
-                    hintText='Ещё не зарегистрированы?'
-                    hintLinkText='Регистрация'
-                >
-                    <Input name='E-mail' type='email'/>
-                    <Input name='Пароль' type='password' error={true}/>
-                </Form>
-            </Auth>
-
-
+            <NotFound />
         </div>
     );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+function NotFound() {
+    return (
+        <section className='NotFound'>
+            <h2 className='NotFound__title'>404</h2>
+            <p className='NotFound__subtitle'>Страница не найдена</p>
+            <a className='NotFound__link' href=''>Назад</a>
+        </section>
+    )
+}
+
+export default NotFound;
