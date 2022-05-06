c08f1157d6a9eb851869bf9234eb0053ca19f578
Avellow <avellow@yandex.ru>
2022-05-06 12:15:56 +0300

    hotfix привести поля ввода в профиле в соответствие с макетом

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,20 +1,27 @@
 function Input(props) {
     const {
         name = 'Поле ввода',
+        className = 'Input',
         type = 'text',
         errored = false,
     } = props;
 
+    const generateClassName = (className) => {
+        return errored
+            ? `${className} ${className}_errored`
+            : className;
+    }
+
     return (
-        <div className='Input'>
-            <label className='Input__name'>
+        <div className={ generateClassName(className) }>
+            <label className={`${className}__name`}>
                 { name }
                 <input
-                    className={`Input__field ${errored && 'Input__field_errored'}`}
+                    className={ generateClassName(`${className}__field`) }
                     type={ type }
                 />
             </label>
-            { errored && <span className='Input__error'>Что-то пошло не так</span> }
+            { errored && <span className={`${className}__error`}>Что-то пошло не так</span> }
         </div>
     )
 }
